8c521644cce7c73df0f5ec82fc803945b83c01e8
jeenadiksha <132073521+jeenadiksha@users.noreply.github.com>
2023-12-30 17:23:42 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -1,9 +1,4 @@
 line = input("Enter a line :")
 sub_string = input("Enter a sub string :")
-sub_counts = 0
-for i in line:
-    if(sub_string in line):
-        sub_counts += 1
-print(sub_counts)
-
-
+sub_counts = line.count(sub_string)
+print(sub_counts
